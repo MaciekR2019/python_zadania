ab2785b02a33926062a9b54e917b55ff193b9944
maciej_rogulski <maciej_rogulski@parp.gov.pl>
2019-09-13 17:11:54 +0200

    Dokompozycja i fikstury w kontaktach v2.

--- fixture/application.py
+++ fixture/application.py
@@ -10,7 +10,7 @@ class Application:
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
-        self.contact = ContactHelper
+        self.contact = ContactHelper(self)
 
     def otworz_strone_startowa(self):
         wd = self.wd
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,29 +11,9 @@ def app(request):
 
 def test_dodaj_kontakt(app):
     app.session.zaloguj(username="admin", password="secret")
-    app.contact.przejdz_do_kontaktow()
-    app.contact.utworz(Contacts(firstname="dfgdfg",
-                                middlename="dfg dfg dfg dg",
-                                lastname="fd gdf gdf gdf",
-                                nickname="dfg df gdfgdg",
-                                title="fgdf gdf gdfg dfgdfg d",
-                                company="fdg dfg dfg dfg",
-                                address="fdg df gdf gdfg dfhfhjgh",
-                                home="fgh fgh fgh fgh",
-                                mobile="fg hfg hfg h",
-                                work="fghfghfghfgjhjghj",
-                                fax="retrytui",
-                                email="hjkjhk jhkhjk hjk",
-                                email2="hjk hjk hjk hjkhjk",
-                                email3="hjk hjk hj",
-                                homepage="fghfghfhgh fgh",
-                                bday="11",
-                                bmonth="April",
-                                byear="1995",
-                                aday="13",
-                                amonth="August",
-                                ayear="2005",
-                                address2="fgh fgh fhfg hf",
-                                phone2="hjkhjkhjkhjk",
+    app.contact.utworz(Contacts(firstname="dfgdfg", middlename="dfg dfg dfg dg", lastname="fd gdf gdf gdf", nickname="dfg df gdfgdg", title="fgdf gdf gdfg dfgdfg d",
+                                company="fdg dfg dfg dfg", address="fdg df gdf gdfg dfhfhjgh", home="fgh fgh fgh fgh", mobile="fg hfg hfg h",
+                                work="fghfghfghfgjhjghj", fax="retrytui", email="hjkjhk jhkhjk hjk", email2="hjk hjk hjk hjkhjk", email3="hjk hjk hj",
+                                homepage="fghfghfhgh fgh", bday="11", bmonth="April", byear="1995", aday="13", amonth="August", ayear="2005", address2="fgh fgh fhfg hf", phone2="hjkhjkhjkhjk",
                                 notes="hjk hkhjkhjk hjkhjk hjk hj"))
     app.session.wyloguj()
